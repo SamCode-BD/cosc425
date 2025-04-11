b86f1116818e1af47158994114ee7ef267b91781
Owen-B3 <ow3n.ross@gmail.com>
2025-04-10 20:41:58 -0400

    Delete react-boneID-prototype directory

--- react-boneID-prototype/src/EditSkeletal.jsx
+++
@@ -1,8 +0,0 @@
-import React, {useState, useContext, useEffect} from 'react'
-import { Context } from './FormContainer';
-
-function EditSkeletal() {
-    return(<div className="bone-form">
-    </div>)
-}
-export default EditSkeletal
--- react-boneID-prototype/src/RightPaneDefault.jsx
+++
@@ -1,4 +0,0 @@
-function RightPaneDefault() {
-    return(<div className='bone-form'></div>);
-}
-export default RightPaneDefault
